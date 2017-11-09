db92da1e75932a2801d8119bbdeb7f102f08e1d3
Eray Akyıldız <akyildiz379@gmail.com>
2017-11-09 22:13:20 +0300

    Django Modelleri Oluşturuldu.

--- DjangoApp/DjangoApp/urls.py
+++ DjangoApp/DjangoApp/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from django.contrib import admin
 from FirstApplication import views
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$',views.AnasayfaSayfaGorunumu.as_view()),
 ]
