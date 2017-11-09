4056ec4be93b4f8b998f5957e4eafaaab39691a0
Eray Akyıldız <akyildiz379@gmail.com>
2017-11-09 22:41:24 +0300

    Database Den Veriler Çekildi.

--- DjangoApp/FirstApplication/views.py
+++ DjangoApp/FirstApplication/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.utils import timezone
+from .models import Gonderi
 
 class AnasayfaSayfaGorunumu(TemplateView):
     def get(self, request, **kwargs):
-        return render(request,'index.html',context=None)
+        gonderiler= Gonderi.objects.filter(y_tarihi__lte=timezone.now()).order_by('y_tarihi')
+        return render(request,'index.html', {'gonderiler':gonderiler} )
